41f2005f74afdd0bc3c7be423b1c3ad0a7f1a6eb
Alexander Kohonovsky <saniaky@gmail.com>
2020-04-03 21:50:24 -0500

    Fix update request.

--- src/app.py
+++ src/app.py
@@ -9,15 +9,6 @@ app = Flask(__name__)
 initialize_db(app)
 
 
-# Tutorial
-# https://github.com/paurakhsharma/flask-rest-api-blog-series/blob/master/Part%20-%201/movie-bag/app.py
-
-# Request
-# https://flask.palletsprojects.com/en/1.1.x/api/#flask.Request
-
-# SQL Examples
-# https://www.bradcypert.com/writing-a-restful-api-in-flask-sqlalchemy/
-
 @app.route('/')
 def hello():
     return {'response': 'Hello world!'}
@@ -40,15 +31,14 @@ def add_movie():
 
 @app.route('/movies/<int:movie_id>', methods=['PUT'])
 def update_movie(movie_id):
-    if (not request.json):
-        return {'error': 'Object is not a JSON', 'request': request.json}, 400
-
-    print(request.json)
+    new_name = request.json['name']
     movie = Movie.query.get(movie_id)
-    movie.name = request.json['name']
+    if (not movie):
+        return {'error': 'Movie not found'}, 400
+    movie.name = new_name
     db.session.add(movie)
     db.session.commit()
-    return jsonify(movie)
+    return jsonify(movie.to_dict())
 
 
 @app.route('/movies/<int:movie_id>', methods=['DELETE'])
--- src/database/models.py
+++ src/database/models.py
@@ -1,5 +1,6 @@
 from .db import db
 
+
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(45), unique=True, nullable=False)
