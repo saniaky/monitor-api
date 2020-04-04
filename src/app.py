8cd5c4ce39be3ad39c5f90901a960a0765963aea
Alexander Kohonovsky <saniaky@gmail.com>
2020-04-04 15:57:35 -0500

    Refactor initialization

--- src/app.py
+++ src/app.py
@@ -10,15 +10,14 @@ from routes.routes import init_routes
 from services.mail_service import init_mail
 
 load_dotenv()
-bcrypt = Bcrypt()
-jwt = JWTManager()
 
 app = Flask(__name__)
+
 app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 app.config['JWT_COOKIE_CSRF_PROTECT'] = False
 
-bcrypt.init_app(app)
-jwt.init_app(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 initialize_db(app)
 init_routes(app)
 init_mail(app)
