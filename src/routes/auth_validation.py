6511b961471e4ded9ea9b99dfdb1965012456695
Alexander Kohonovsky <saniaky@gmail.com>
2020-04-21 11:57:25 -0500

    Add profile update

--- src/routes/auth_validation.py
+++ src/routes/auth_validation.py
@@ -6,7 +6,7 @@ from database.user import User
 first_name_validate = validate.Length(min=1, max=45)
 last_name_validate = validate.Length(min=1, max=45)
 email_validate = validate.Email()
-avatar_validate = validate.URL(relative=False, schemes=['http', 'https'])
+avatar_validate = validate.Length(max=45)
 password_validate = validate.Length(min=5, max=45)
 
 
@@ -39,4 +39,4 @@ class UpdateProfileSchema(Schema):
     first_name = fields.Str(required=False, validate=first_name_validate)
     last_name = fields.Str(required=False, validate=last_name_validate)
     password = fields.Str(required=False, validate=password_validate)
-    avatar_url = fields.Str(required=False, validate=avatar_validate)
+    avatar_url = fields.Str(required=False)
